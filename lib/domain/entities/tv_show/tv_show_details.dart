767a881bc6024cc5fca92b8c51894026820b266e
Cristhian Rodriguez <cprodriguez@unitru.edu.pe>
2024-08-02 22:24:10 -0500

    add tv_show_details

---
+++ lib/domain/entities/tv_show/tv_show_details.dart
@@ -0,0 +1,59 @@
+import 'package:equatable/equatable.dart';
+import 'package:clean_arch_movie_flutter/domain/entities/util/cast.dart';
+import 'package:clean_arch_movie_flutter/domain/entities/util/review.dart';
+import 'package:clean_arch_movie_flutter/domain/entities/tv_show/season.dart';
+
+class TvShowDetailsEntity extends Equatable {
+  final int? id;
+  final String? title;
+  final String? posterUrl;
+  final String? backdropUrl;
+  final String? releaseDate;
+  final String? genres;
+  final String? runtime;
+  final int? numberOfSeasons;
+  final String? overview;
+  final double? voteAverage;
+  final String? voteCount;
+  final String? trailerUrl;
+  final List<CastEntity>? cast;
+  final List<ReviewEntity>? reviews;
+  final List<SeasonEntity>? seasons;
+
+  const TvShowDetailsEntity({
+    this.id,
+    this.title,
+    this.posterUrl,
+    this.backdropUrl,
+    this.releaseDate,
+    this.genres,
+    this.runtime,
+    this.numberOfSeasons,
+    this.overview,
+    this.voteAverage,
+    this.voteCount,
+    this.trailerUrl,
+    this.cast,
+    this.reviews,
+    this.seasons,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        title,
+        posterUrl,
+        backdropUrl,
+        releaseDate,
+        genres,
+        runtime,
+        numberOfSeasons,
+        overview,
+        voteAverage,
+        voteCount,
+        trailerUrl,
+        cast,
+        reviews,
+        seasons,
+      ];
+}
