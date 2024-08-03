ba5211b6dda8f39c4f2a7a1a829dc9ba9421c812
Cristhian Rodriguez <cprodriguez@unitru.edu.pe>
2024-08-02 20:40:05 -0500

    add entities tv_show

---
+++ lib/domain/entities/tv_show/season_details.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+import './episode.dart';
+
+class SeasonDetails extends Equatable {
+  final List<Episode> episodes;
+
+  const SeasonDetails({
+    required this.episodes,
+  });
+
+  @override
+  List<Object?> get props => [
+        episodes,
+      ];
+}
