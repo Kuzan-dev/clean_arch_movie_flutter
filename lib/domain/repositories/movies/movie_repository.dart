fc5adc8e1b08d30d26ded9da417f1abe9ad59ec9
Kuzan-dev <cjuarezr99@gmail.com>
2024-08-03 11:49:00 -0500

    movie and util repositories

---
+++ lib/domain/repositories/movies/movie_repository.dart
@@ -0,0 +1,35 @@
+import 'package:clean_arch_movie_flutter/core/exceptions/database_exception.dart';
+import 'package:clean_arch_movie_flutter/core/exceptions/network_exception.dart';
+import 'package:clean_arch_movie_flutter/domain/entities/movies/movie_details.dart';
+import 'package:clean_arch_movie_flutter/domain/entities/movies/movie_list.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract class MovieRepository{
+  //Data Remota
+  // Get top rated movies
+  Future<Either<NetworkException, MovieListEntity>>getTopRatedMovies({required int page});
+
+  // Get popular movies
+  Future<Either<NetworkException, MovieListEntity>>getPopularMovies({required int page});
+
+  // Get details of a movie
+  Future<Either<NetworkException, MovieDetailsEntity>>getMovieDetails({required int movieId});
+
+  // Get similar movies
+  Future<Either<NetworkException, List<MovieListEntity>>>getSimilarMovies({required int movieId, required int page});
+
+
+  //Data Local
+  //Get favorite movies
+  Future<Either<DatabaseException, List<MovieDetailsEntity>>> getFavoriteMovies();
+
+  //Save a movie detail as favorite
+  Future<Either<DatabaseException, void>>saveMovieDetail({required MovieDetailsEntity? movieDetailsEntity});
+
+  //Remove a movie detail from favorite
+  Future<Either<DatabaseException, void>>removeMovieDetail({required int? movieId});
+
+  //Check if a movie detail is favorite
+  Future<Either<DatabaseException, bool>>isFavorite({required int? movieId});
+
+}
---
+++ lib/domain/repositories/util/util_repository.dart
@@ -0,0 +1,18 @@
+import 'package:clean_arch_movie_flutter/core/exceptions/network_exception.dart';
+import 'package:clean_arch_movie_flutter/domain/entities/util/actor.dart';
+import 'package:clean_arch_movie_flutter/domain/entities/util/actor_movies.dart';
+import 'package:clean_arch_movie_flutter/domain/entities/util/cast.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract class UtilRepository{
+  //Data Remota
+  // Get cast of a movie or tv show
+  Future<Either<NetworkException, List<CastEntity>>>getCast({required int id});
+
+  // Get actor details
+  Future<Either<NetworkException, ActorDetailEntity>>getActorDetails({required int id});
+
+  // Get actor movies
+  Future<Either<NetworkException, List<ActorMovieEntity>>>getActorMovies({required int id});
+  
+}
