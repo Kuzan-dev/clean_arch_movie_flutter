5f535634f5ca597a833244929385b87a7ae50d0c
Kuzan-dev <cjuarezr99@gmail.com>
2024-08-03 12:11:32 -0500

    remove media

--- lib/domain/repositories/movies/movie_repository.dart
+++ lib/domain/repositories/movies/movie_repository.dart
@@ -4,32 +4,37 @@ import 'package:clean_arch_movie_flutter/domain/entities/movies/movie_details.da
 import 'package:clean_arch_movie_flutter/domain/entities/movies/movie_list.dart';
 import 'package:fpdart/fpdart.dart';
 
-abstract class MovieRepository{
+abstract class MovieRepository {
   //Data Remota
   // Get top rated movies
-  Future<Either<NetworkException, MovieListEntity>>getTopRatedMovies({required int page});
+  Future<Either<NetworkException, MovieListEntity>> getTopRatedMovies(
+      {required int page});
 
   // Get popular movies
-  Future<Either<NetworkException, MovieListEntity>>getPopularMovies({required int page});
+  Future<Either<NetworkException, MovieListEntity>> getPopularMovies(
+      {required int page});
 
   // Get details of a movie
-  Future<Either<NetworkException, MovieDetailsEntity>>getMovieDetails({required int movieId});
+  Future<Either<NetworkException, MovieDetailsEntity>> getMovieDetails(
+      {required int movieId});
 
   // Get similar movies
-  Future<Either<NetworkException, List<MovieListEntity>>>getSimilarMovies({required int movieId, required int page});
-
+  Future<Either<NetworkException, List<MovieListEntity>>> getSimilarMovies(
+      {required int movieId, required int page});
 
   //Data Local
   //Get favorite movies
-  Future<Either<DatabaseException, List<MovieDetailsEntity>>> getFavoriteMovies();
+  Future<Either<DatabaseException, List<MovieDetailsEntity>>>
+      getFavoriteMovies();
 
   //Save a movie detail as favorite
-  Future<Either<DatabaseException, void>>saveMovieDetail({required MovieDetailsEntity? movieDetailsEntity});
+  Future<Either<DatabaseException, void>> saveMovieDetail(
+      {required MovieDetailsEntity? movieDetailsEntity});
 
   //Remove a movie detail from favorite
-  Future<Either<DatabaseException, void>>removeMovieDetail({required int? movieId});
+  Future<Either<DatabaseException, void>> removeMovieDetail(
+      {required int? movieId});
 
   //Check if a movie detail is favorite
-  Future<Either<DatabaseException, bool>>isFavorite({required int? movieId});
-
+  Future<Either<DatabaseException, bool>> isFavorite({required int? movieId});
 }
--- lib/domain/repositories/util/util_repository.dart
+++ lib/domain/repositories/util/util_repository.dart
@@ -4,15 +4,16 @@ import 'package:clean_arch_movie_flutter/domain/entities/util/actor_movies.dart'
 import 'package:clean_arch_movie_flutter/domain/entities/util/cast.dart';
 import 'package:fpdart/fpdart.dart';
 
-abstract class UtilRepository{
+abstract class UtilRepository {
   //Data Remota
   // Get cast of a movie or tv show
-  Future<Either<NetworkException, List<CastEntity>>>getCast({required int id});
+  Future<Either<NetworkException, List<CastEntity>>> getCast({required int id});
 
   // Get actor details
-  Future<Either<NetworkException, ActorDetailEntity>>getActorDetails({required int id});
+  Future<Either<NetworkException, ActorDetailEntity>> getActorDetails(
+      {required int id});
 
   // Get actor movies
-  Future<Either<NetworkException, List<ActorMovieEntity>>>getActorMovies({required int id});
-  
+  Future<Either<NetworkException, List<ActorMovieEntity>>> getActorMovies(
+      {required int id});
 }
