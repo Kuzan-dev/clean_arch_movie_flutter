882fed63db9d07ed58544c29c7d370c3efddeb8d
Kuzan-dev <cjuarezr99@gmail.com>
2024-08-03 21:22:56 -0500

    url_constants created

---
+++ lib/data/datasources/remote/movies/movie_remote_data_source.dart
@@ -0,0 +1,13 @@
+
+import 'package:clean_arch_movie_flutter/data/models/movies/movie_details_model.dart';
+import 'package:clean_arch_movie_flutter/data/models/movies/movie_list_model.dart';
+
+abstract class MovieRemoteDataSource {
+  Future<MovieListModel>getTopRatedMovies({required int page});
+
+  Future<MovieListModel>getPopularMovies({required int page});
+
+  Future<MovieDetailsModel>getMovieDetails({required int movieId});
+
+  Future<List<MovieDetailsModel>>getSimilarMovies({required int movieId, required int page});
+}
