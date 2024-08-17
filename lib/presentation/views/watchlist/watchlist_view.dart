e4ef756e4e1636bdf07f10876f560cff30f62b06
Cristhian Rodriguez <cprodriguez@unitru.edu.pe>
2024-08-17 02:25:07 -0500

    add watchlist view

--- lib/presentation/views/watchlist/watchlist_view.dart
+++ lib/presentation/views/watchlist/watchlist_view.dart
@@ -1,6 +1,8 @@
 import 'package:clean_arch_movie_flutter/core/components/carrousell/section_listview_card.dart';
+import 'package:clean_arch_movie_flutter/presentation/controllers/watchlist/watchlist_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
 class WatchlistView extends StatelessWidget {
@@ -9,35 +11,62 @@ class WatchlistView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+        body: MultiBlocListener(
+      listeners: [
+        BlocListener<WatchlistCubit, WatchListState>(
+          listener: (context, state) {
+            if (state is WatchListError) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(state.message)),
+              );
+            }
+          },
+        ),
+      ],
+      child: Column(
         children: [
           const Padding(padding: EdgeInsets.only(top: 30)),
           const Text('Watchlist'),
-          Expanded(
-              child: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(14, 0, 14, 8),
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: 2,
-                    mainAxisSpacing: 1,
-                    childAspectRatio: 0.44,
-                  ),
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.fromLTRB(3, 2, 3, 2),
-                      child: Text('Watchlist Item $index'),
-                    );
-                  },
-                  itemCount: 10,
-                ),
-              )
-            ],
+          Expanded(child: BlocBuilder<WatchlistCubit, WatchListState>(
+            builder: (context, watchlistState) {
+              if (watchlistState is WatchlistLoading &&
+                  watchlistState is! WatchListLoaded) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (watchlistState is WatchListLoaded) {
+                final media = watchlistState.media;
+                return Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(14, 0, 14, 8),
+                      child: GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 2,
+                          mainAxisSpacing: 1,
+                          childAspectRatio: 0.44,
+                        ),
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.fromLTRB(3, 2, 3, 2),
+                            child: SectionListViewCard(
+                              media: media[index],
+                              isMovie: media[index].isMovie,
+                            ),
+                          );
+                        },
+                        itemCount: 10,
+                      ),
+                    )
+                  ],
+                );
+              } else {
+                return const Center(child: Text('No movies in watchlist'));
+              }
+            },
           ))
         ],
       ),
-    );
+    ));
   }
 }
